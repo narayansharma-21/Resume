fe8aed59346bd09d492ca97c53dc52c79c8b38ad
narayansharma-21 <97.sharman@gmail.com>
2023-09-10 10:56:34 -0400

    Updated script

--- script.py
+++ script.py
@@ -1,35 +1,19 @@
-# Basic script to download resume and override existing one 
-
-import requests
 import shutil
 import os
 
-# URL of the PDF file you want to download
-resume_url = "/Users/kingnutmegs/Documents/GitHub/Resume/Narayan Sharma Resume 2023.pdf"
+# Source file path (your local file)
+source_file_path = "/Users/kingnutmegs/Documents/GitHub/Resume/Narayan Sharma Resume 2023.pdf"
 
-# Path to your Downloads directory
+# Destination directory (your Downloads directory)
 downloads_dir = os.path.expanduser("~/Downloads")
 
 # Define the name of the PDF file
 pdf_file_name = "Narayan Sharma Resume.pdf"
 
-# Download the PDF file
-response = requests.get(resume_url, stream=True)
+# Create the full path to the destination file in the Downloads directory
+destination_pdf_path = os.path.join(downloads_dir, pdf_file_name)
 
-# Check if the download was successful (HTTP status code 200)
-if response.status_code == 200:
-    # Create the full path to the new PDF file in the Downloads directory
-    pdf_path = os.path.join(downloads_dir, pdf_file_name)
+# Copy the source file to the destination directory
+shutil.copyfile(source_file_path, destination_pdf_path)
 
-    # Save the downloaded file to the Downloads directory
-    with open(pdf_path, "wb") as pdf_file:
-        shutil.copyfileobj(response.raw, pdf_file)
-
-    # Remove the previous version of the resume (if it exists)
-    previous_resume_path = os.path.join(downloads_dir, pdf_file_name)
-    if os.path.exists(previous_resume_path):
-        os.remove(previous_resume_path)
-
-    print("Resume has been updated and saved to Downloads directory.")
-else:
-    print("Failed to download the resume PDF. Check the URL or your internet connection.")
+print("Resume has been copied to Downloads directory.")
